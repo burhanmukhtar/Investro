f69e3fd99f85bd0cc8f44e5de136a3d7add5434d
Burhan mukhtar <62393908+burhanmukhtar@users.noreply.github.com>
2025-03-27 01:07:41 +0530

    asda

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 # run.py
-from app import create_app, db, socketio
+from app import create_app, db
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    # Use socketio.run instead of app.run to enable WebSocket support
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
